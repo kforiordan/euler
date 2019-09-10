8676286fbe8c1cb97bd98954b2cad26ca311c0a2
Kevin O'Riordan <kevin@oriordan.org>
2019-09-10 10:06:05 +0100

    broken efforts at Euler 004

--- 004-largest-palindrome-product/004.ml
+++ 004-largest-palindrome-product/004.ml
@@ -19,23 +19,48 @@ let prods lwr upr =
   in
   aux upr;;
 
+let magnitude x = int_of_float (log10 (float_of_int x));;
+
+(* Most Significant Figure. *)
+let msf x = x / int_of_float (10. ** float_of_int (magnitude x));;
+
+(* Least Significant Figure. *)
+let lsf x = x mod 10;;
+
+(* The inner part of a number.  Eg. 98765 -> 876; 1234 -> 23.  Oh no!
+   This fails with zeroes.  Belphegor's number becomes
+   6660000000000000, not 00000000000006660000000000000. *)
+let inner x =
+  let m = magnitude x in
+  let leftmost = (msf x) * int_of_float((10. ** float_of_int (m)))
+  and rightmost = lsf x
+  in
+  ((x - rightmost) - leftmost) / 10;;
+
 let rec is_palindrome x =
-  if x < 10
-  then
-    true
-  else
-    let x' = float_of_int x
-    in (
-      let msf = ( let magnitude = float_of_int (int_of_float (log10 x'))
-                  in x / int_of_float (10. ** magnitude) )
-        and
-          lsf = x mod 10
-          in
-          (print_int msf; print_string " "; print_int lsf; print_string "\n"; true)
-    );;
+  let m = magnitude x
+  in
+  let fig n = 0
+
+  let rec aux  i =
+    if x' < 10
+    then
+      true
+    else
+      let m = (fig ) and l = (lsf x')
+      in
+      if m = l
+      then (* recurse *)
+      else false
+  in aux 0;;
+
+let belphegor = 1000066600001;; (* Well, sorta. *)
 
 let _ = is_palindrome 12321;;
 let _ = is_palindrome 82321;;
+let _ = is_palindrome 1234;;
+let _ = is_palindrome 12345;;
+let _ = is_palindrome belphegor;;
 
 let _ = let sorted_prods =
           List.sort (fun x y -> if x > y then -1 else 1) (prods 100 102)
