7d81e226418a05900df9272aa9e884af6de4feee
Kevin O'Riordan <kevin@oriordan.org>
2019-09-10 12:31:00 +0100

    Solve Euler 006 (Ocaml)

--- 006-sum-square-difference/006.ml
+++ 006-sum-square-difference/006.ml
@@ -11,3 +11,16 @@
 
    Find the difference between the sum of the squares of the first one
    hundred natural numbers and the square of the sum. *)
+
+let gauss n = (n * (n+1)) / 2;;
+
+let rec seq a b = if a > b then [] else a :: seq (a + 1) b;;
+
+let x = 100
+
+let sum_of_squares = List.fold_left (fun x y -> x + y) 0
+                       (List.map (fun x -> x * x) (seq 1 x));;
+
+let square_of_sums = let g = gauss x in g * g;;
+
+let diff = square_of_sums - sum_of_squares;;
