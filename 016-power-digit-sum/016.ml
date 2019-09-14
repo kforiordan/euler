a200fd72e42266bd1b68551d4bff4b7d155c1f28
Kevin O'Riordan <kevin@oriordan.org>
2019-09-14 22:21:12 +0100

    Solve Euler 016 (Ocaml)

--- 016-power-digit-sum/016.ml
+++ 016-power-digit-sum/016.ml
@@ -2,3 +2,19 @@
 
    What is the sum of the digits of the number 2^1000? *)
 
+#require "zarith";;
+
+let f x =
+  let rec aux x' digits =
+    if Z.Compare.(=) x' Z.zero
+    then digits
+    else
+      let (d, r) = Z.div_rem x' (Z.of_int 10)
+      in
+      aux d ((Z.to_int r) :: digits)
+  in
+  aux x [];;
+
+let target = Z.pow (Z.of_int 2) 1000;;
+
+let sum = List.fold_left (fun x y -> x + y) 0 (f target)
