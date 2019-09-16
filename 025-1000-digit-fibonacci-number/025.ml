2a956a35cafaef77d6eea04df3b8a6e015277662
Kevin O'Riordan <kevin@oriordan.org>
2019-09-16 21:23:15 +0100

    Solve Euler 025 (Ocaml)
    Zarith based solution, but this version of the module is different to
    the version I'm using on macos.  Z.equal here, Z.Compare.(=) there.
    
    How is this an improvement on the old arbitrary precision modules that
    came with ocaml?  It's all GMP under the hood, afaik.

--- 025-1000-digit-fibonacci-number/025.ml
+++ 025-1000-digit-fibonacci-number/025.ml
@@ -28,17 +28,38 @@ What is the index of the first term in the Fibonacci sequence to contain 1000 di
 #require "zarith";;
 
 let fib n =
-  let first_fib = (Z.of_int 1) and second_fib = (Z.of_int 2)
+  let first_fib = 1 and second_fib = 2
   in
   let rec aux n' acc1 acc2 =
-    if Z.Compare.(=) n Z.zero
-    then first_fib
-    else if Z.Compare.(=) n (Z.of_int 1)
-    then second_fib
-    else if Z.Compare.(=) n' n
-    then Z.add acc1 acc2
-    else aux (Z.succ n') acc2 (Z.add acc1 acc2)
+    match n with
+      0 -> first_fib
+    | 1 -> second_fib
+    | _ -> if n' = n
+           then acc1 + acc2
+           else aux (n' + 1) acc2 (acc1 + acc2)
+  in aux 2 first_fib second_fib;;
+
+let fib n =
+  let first_fib = (Z.of_int 1) and second_fib = (Z.of_int 1)
+  in
+  let rec aux n' acc1 acc2 =
+    if Z.equal n Z.zero then first_fib
+    else if Z.equal n (Z.of_int 1) then second_fib
+    else
+      if Z.equal n' n
+      then Z.add acc1 acc2
+      else aux (Z.succ n') acc2 (Z.add acc1 acc2)
   in aux (Z.of_int 2) first_fib second_fib;;
 
-(Z.of_int 1) = Z.succ (Z.succ Z.zero);;
 
+let solve n =
+  let rec aux i =
+    let s = Z.to_string (fib (Z.of_int i)) in
+    if String.length s >= n
+    then (i+1)
+    else aux (i+1) (* I've zero-indexed the sequence; we want a 1-indexed solution. *)
+  in aux 0;;
+
+let target = 1000;;
+
+let solution = solve target;;
