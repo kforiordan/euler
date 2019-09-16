cd017de2b469e24523348d9573de97db4d0b0a1e
Kevin O'Riordan <kevin@oriordan.org>
2019-09-16 17:47:58 +0100

    Add broken arbitrary precision fibonacci function
    Uses Zarith but, oh no, Z.Compare can't be found here on Ubuntu.  Only
    works on my mac.  I'm using Zarith because bignum only works on Ubuntu,
    not on my mac.  If only there was one module that would work on both.

--- 025-1000-digit-fibonacci-number/025.ml
+++ 025-1000-digit-fibonacci-number/025.ml
@@ -21,3 +21,24 @@ The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits? *)
 
+
+(* Tail-recursive O(n) fibonacci function borrowed from 002, modified
+   to work with arbitrarily large numbers. *)
+
+#require "zarith";;
+
+let fib n =
+  let first_fib = (Z.of_int 1) and second_fib = (Z.of_int 2)
+  in
+  let rec aux n' acc1 acc2 =
+    if Z.Compare.(=) n Z.zero
+    then first_fib
+    else if Z.Compare.(=) n (Z.of_int 1)
+    then second_fib
+    else if Z.Compare.(=) n' n
+    then Z.add acc1 acc2
+    else aux (Z.succ n') acc2 (Z.add acc1 acc2)
+  in aux (Z.of_int 2) first_fib second_fib;;
+
+(Z.of_int 1) = Z.succ (Z.succ Z.zero);;
+
