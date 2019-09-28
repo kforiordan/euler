837a5b566af91855a61fe58623b173e5ffcd0c05
Kevin O'Riordan <kevin@oriordan.org>
2019-09-28 13:14:05 +0100

    Minimal progress on Euler 034

--- 034-digit-factorials/034.ml
+++ 034-digit-factorials/034.ml
@@ -6,3 +6,16 @@
    Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 *)
 
+let rec digits n = if n < 10
+                   then [n]
+                   else (n mod 10) :: (digits (n / 10));;
+
+let rec factorial n =  if n = 1
+                       then 1
+                       else n * factorial (n - 1);;
+
+let sum_of_factorials_of_digits n =
+  List.fold_left (+) 0 (List.map (factorial) (digits n));;
+
+(* Now to find all the other curious numbers.  I'll just brute force
+   my way through the naturals. *)
