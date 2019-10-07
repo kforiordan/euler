ce0d000cf4ae5ab1588e43ef8ca3bf930617b6f7
Kevin O'Riordan <kevin@oriordan.org>
2019-10-07 17:21:01 +0100

    Oh no, don't look at this
    Sometimes I just use git to move work from computer A to computer B.

--- 243-resilience/243.ml
+++ 243-resilience/243.ml
@@ -70,7 +70,7 @@ let next_prime n =
     let rec aux i = if is_prime i then i else aux (i + 2)
     in aux (n + 2);;
 
-let solve target =
+let solve_computationally target =
   let rec aux factors =
     let candidate = List.fold_left ~f:( * ) ~init:1 factors in
     let _ = print_string ((string_of_int candidate) ^ "\n") in
@@ -87,8 +87,8 @@ let solve target =
 
 let target_resilience = 1.0 *. (15499. /. 94744.);;
 
-let (solution, resilience) = solve target_resilience in
-    (target_resilience, solution, resilience);;
+(* let (solution, resilience) = solve target_resilience in
+ *     (target_resilience, solution, resilience);; *)
 
 
 (* My first idea was to try each number that has only prime factors,
@@ -100,3 +100,71 @@ let (solution, resilience) = solve target_resilience in
    each input, isn't going to work here - or, rather, it could be made
    to work, but wouldn't finish within a minute.  I need a different
    approach. *)
+
+(* I'm sure I'm right that the solution is a product of prime factors,
+   but the trick is to reason about how much each additional prime
+   factor affects resilience, and to just keep adding them until we've
+   reached the threshold.
+
+   So: for any number divisible by the first prime, 2, almost 50% of
+   its proper fractions are non-resilient.  Any number divisible by
+   both 2 and the second prime, 3, will have almost 50% + (50% of
+   33.3%) of its proper fractions non-resilient.  Add 5 as a factor
+   and we're at 50% + (50% of 33.3%) + (50% of 33.3% of 20%) ... and
+   so on.  I'm guessing the product of the first 9 or 10 primes is the
+   solution. *)
+
+let resilience_quotient_factors factors =
+  let things =
+    let rec aux porosity' factors' =
+      match factors' with
+        [] -> []
+      | hd :: tl -> (List.fold_left ~f:( * ) ~init:1 factors') :: (aux tl)
+    in aux factors
+  in
+  things;;
+  List.map ~f:(fun x -> 1. /. (float_of_int x)) things;;
+
+let _ = resilience_quotient_factors [5;3;2];;
+
+  let porosity =
+    let aux factors' =
+      match factors' with
+        [] -> 0.
+      | hd :: tl ->
+         1. /. (float_of_int (List.fold_left ~f:( * ) ~init:1 factors')
+
+
+let solve_mathematically target =
+  let rec aux factors =
+    if resilience_quotient_factors factors < target
+    then List.fold_left ~f:( * ) ~init:1 factors
+    else
+      match factors with
+        [] -> aux [2]
+      | hd :: _ -> aux (next_prime hd :: factors)
+  in aux [];;
+
+let solve = solve_mathematically;;
+
+
+(* Some exploratory stuff I should've done at the outset: *)
+
+(* Borrowed from 021 *)
+let proper_divisors x =
+  let is_proper_divisor n d = n > d && n mod d = 0 in
+  let rec aux i divisors =
+    if i >= x
+    then divisors
+    else
+      if is_proper_divisor x i
+      then aux (i+1) (i :: divisors)
+      else aux (i+1) divisors
+  in aux 1 [];;
+
+let _ = proper_divisors 15499;;
+let _ = proper_divisors 94744;;
+let _ = euclid_gcd 15499 94744;;
+
+(* Result: 15499 and 94744 are coprime.  I can't see a reason why
+   these particular numbers were chosen. *)
