debaa977c71706f7b22d25180c139ab8dc8fd17b
Kevin O'Riordan <kevin@oriordan.org>
2020-02-23 11:06:36 +0000

    A first guess.

--- 015-lattice-paths/015.ml
+++ 015-lattice-paths/015.ml
@@ -3,3 +3,18 @@
    bottom right corner.
 
    How many such routes are there through a 20×20 grid? *)
+
+(* Based on drawing out the 4x4 solutions, I think it's (2^n - 2), so
+   let's work that out. *)
+
+#require "zarith";;
+
+let solve_for n =
+  let two = Z.of_int 2 in Z.sub (Z.pow two n) two;;
+
+let print_z z =
+  print_string ((Z.to_string z) ^ "\n");;
+
+let _ = print_z (solve_for 20);;
+
+(* 2^20 - 2 is not the right answer. *)
