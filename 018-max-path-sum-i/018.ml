b3d3af5da3368e33c7fc1c2051192473a8a195ae
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2020-03-04 17:16:52 +0000

    scratchings

--- 018-max-path-sum-i/018.ml
+++ 018-max-path-sum-i/018.ml
@@ -32,3 +32,35 @@
    and requires a clever method! ;o)
 *)
 
+#require "core.top";;
+open Core;;
+
+let list_from_line line =
+  Base.String.split ~on:' ' line;;
+
+let list_from_file filename =
+  let lines = In_channel.read_lines filename
+  in
+  List.fold_left ~f:(@) ~init:[]
+    (List.map ~f:list_from_line lines);;
+
+let triangle = tree_from_file "triangle-small.txt"
+
+type 'a bt =
+  | Empty
+  | Node of 'a bt * 'a * 'a bt
+
+(* bfs is actually harder than it sounds.
+
+let rec add_to_tree tree n =
+  match tree with
+    Empty -> Node (Empty, n, Empty)
+  | Node (Empty, n', right) -> Node ((add_to_tree Empty n), n', Empty)
+  | Node (left, n', Empty) -> Node (left, n', (add_to_tree Empty n))
+  | Node (left, n', right) -> Node (add_to_tree
+
+let tree_from_list tree list =
+  match list with
+    [] -> tree
+  | hd :: tl -> add_to_tree hd 
+*)
