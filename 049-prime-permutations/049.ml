d5c6ce26de5c3e6c5a354447f51f3816fcd94509
Kevin O'Riordan <kevin@oriordan.org>
2020-10-06 23:15:21 +0100

    solve euler 049 (ocaml)

--- 049-prime-permutations/049.ml
+++ 049-prime-permutations/049.ml
@@ -41,7 +41,7 @@ let digits n =
       in aux n'' (r :: digits)
   in aux n [];;
 
-o(* The inverse of the digits function. *)
+(* The inverse of the digits function. *)
 let combine digits =
   let rec aux digits power =
     match digits with
@@ -51,10 +51,59 @@ let combine digits =
   in aux (List.rev digits) 0;;
 
 
+
+
+
 (* It took me far too long to write this permutation function.  It
    could be optimised - memoization is the first thing I can think of
    - but not tonight. *)
-let rec permute l =
+let permute l =
+  let rec listcmp metacmp lx ly =
+    match lx, ly with
+      [], [] -> 0
+    | _, [] -> 1
+    | [], _ -> -1
+    | x :: xs, y :: ys ->
+       let r = metacmp x y in
+       if r = 0
+       then listcmp metacmp xs ys
+       else r
+  in
+  let intlistcmp =
+    let cmp a b = if a < b then -1 else if a > b then 1 else 0 in
+    listcmp cmp
+  in
+  let uniq cmp l =
+    let rec aux prev l' =
+      match l' with
+        [] -> []
+      | x :: xs ->
+         match prev with
+           None -> x :: aux (Some x) xs
+         | Some prev' ->
+            if cmp x prev' = 0
+            then aux (Some prev') xs
+            else x :: aux (Some x) xs
+    in
+    aux None (List.sort ~compare:cmp l)
+  in
+  let rec aux l' =
+    let cap_lists head tails = List.map ~f:(fun x -> head :: x) tails in
+    match l' with
+      [] -> []
+    | x :: [] -> [[x]]
+    | x :: x' :: [] -> [[x;x'];[x';x]]
+    | x :: xs ->
+       let rec adj prev remaining =
+         match remaining with
+           [] -> []
+         | x :: remaining' ->
+            (cap_lists x (aux (prev @ remaining'))) @ adj (x :: prev) remaining'
+       in adj [] l'
+  in
+  uniq intlistcmp (aux l);;
+
+let rec permute_naively l =
   let cap_lists head tails = List.map ~f:(fun x -> head :: x) tails in
   match l with
     [] -> []
@@ -65,59 +114,115 @@ let rec permute l =
        match remaining with
          [] -> []
        | x :: remaining' ->
-          (cap_lists x (permute (prev @ remaining'))) @ adj (x :: prev) remaining'
+          (cap_lists x (permute_naively (prev @ remaining'))) @ adj (x :: prev) remaining'
      in adj [] l;;
 
-
-let is_palindrome a b = a = combine (List.rev (digits b));;
-
-(* We can strip out any primes containing zeroes. *)
-let contains_zero n =
-  let f b i = b || i = 0 in
-  let d = digits n in
-  List.fold ~init:false ~f:f d;;
-
-(* And we can strip out any primes containing two or more even numbers. *)
-let contains_two_evens n =
-  let is_even i = i % 2 = 0 in
-  let f b i = b + (if is_even i then 1 else 0) in
-  let d = digits n in
-  if (List.fold ~init:0 ~f:f d) >= 2
-  then true
-  else false;;
-
-let primes = List.filter ~f:is_prime (List.range ~stride:2 1001 10000);;
-
-let pair n =
-  let cmp a b = if a < b then -1 else if a = b then 0 else 1 in
-  let sort = List.sort ~compare:cmp in
-  combine (sort (digits n));;
-
-let hmm =
-  let cmp (a,a') (b,b') = if a < b then -1 else if a = b then 0 else 1 in
+let prime_permutations n =
+  let cmp a b = if a < b then -1 else if a > b then 1 else 0 in
   List.sort ~compare:cmp
-    (List.filter ~f:(fun (a,b) -> a > 1000)
-       (List.map ~f:(fun x -> (pair x), x) primes));;
+    (List.filter ~f:is_prime
+       (List.map ~f:combine (permute (digits n))));;
 
+let solve i limit =
+  let i' = i + ((i+1) % 2) in
+  let range = List.range ~stride:2 i' limit in
+  let primes = List.filter ~f:is_prime range in
+  let related_primes = List.map ~f:prime_permutations primes in
+  let size_filter = List.filter ~f:(fun x -> x >= i && x < limit) in
+  let related_primes' = List.map ~f:(fun l -> size_filter l) related_primes in
+  uniq intlistcmp related_primes';;
 
-let rec f l =
-  let rec aux prev l' =
-    match l' with
-      [] -> []
-    | (i,n) :: tl ->
-       if i = prev
-       then n :: aux i tl
-       else aux i tl
-  in
-  aux 0 l;;
+let hmm = solve 1000 10000;;
 
+let rec the3300 l =
+  match l with
+    [] -> []
+  | hd :: [] -> []
+  | hd :: hd' :: [] -> []
+  | hd :: hd' :: hd'' :: tl -> if hd' - hd = hd'' - hd'
+                       then (hd, hd', hd'') :: the3300 (hd' :: hd'' :: tl)
+                       else the3300 (hd' :: hd'' :: tl);;
 
-let lol = List.map ~f:permute (List.map ~f:digits primes);;
+let rec the3300' l =
+  match l with
+    [] -> false
+  | hd :: [] -> false
+  | hd :: hd' :: [] -> false
+  | hd :: hd' :: hd'' :: tl ->
+       if hd' - hd = hd'' - hd'
+       then true (* (hd, hd', hd'') :: the3300 (hd' :: hd'' :: tl) *)
+       else the3300' (hd' :: hd'' :: tl);;
 
-let rofl = List.map ~f:(fun x -> List.map ~f:combine x) lol;;
+let solution = solve 1000 10000;;
+let solns = List.filter ~f:the3300' solution;;
 
-let foo = List.map ~f:(fun x -> List.filter ~f:(fun y -> y > 1000) x) rofl;;
+(* At this point I noticed that the difference is 3330, not 3300.  The answer is
 
-let bar = List.map ~f:(fun x -> List.filter ~f:is_prime x) foo;;
+     2969 ^ 6299 ^ 9629
+*)
 
-let quux = List.filter ~f:(fun x -> List.length x >= 3) bar;;
+(* Detritus from here on down, kept for posterity.
+
+ * let _ = prime_permutations 1006;;
+ * 
+ * let _ = is_prime 1006;;
+ * 
+ * let urgh = List.map ~f:digits (List.range ~stride:2 1001 1008);;
+ * 
+ * let hmm = List.map ~f:permute_naively urgh;;
+ * let hmm = List.map ~f:permute urgh;;
+ * 
+ * 
+ * let is_palindrome a b = a = combine (List.rev (digits b));;
+ * 
+ * (\* We can strip out any primes containing zeroes. *\)
+ * let contains_zero n =
+ *   let f b i = b || i = 0 in
+ *   let d = digits n in
+ *   List.fold ~init:false ~f:f d;;
+ * 
+ * (\* And we can strip out any primes containing two or more even numbers. *\)
+ * let contains_two_evens n =
+ *   let is_even i = i % 2 = 0 in
+ *   let f b i = b + (if is_even i then 1 else 0) in
+ *   let d = digits n in
+ *   if (List.fold ~init:0 ~f:f d) >= 2
+ *   then true
+ *   else false;;
+ * 
+ * let primes = List.filter ~f:is_prime (List.range ~stride:2 1001 10000);;
+ * 
+ * let pair n =
+ *   let cmp a b = if a < b then -1 else if a = b then 0 else 1 in
+ *   let sort = List.sort ~compare:cmp in
+ *   combine (sort (digits n));;
+ * 
+ * let hmm =
+ *   let cmp (a,a') (b,b') = if a < b then -1 else if a = b then 0 else 1 in
+ *   List.sort ~compare:cmp
+ *     (List.filter ~f:(fun (a,b) -> a > 1000)
+ *        (List.map ~f:(fun x -> (pair x), x) primes));;
+ * 
+ * 
+ * let rec f l =
+ *   let rec aux prev l' =
+ *     match l' with
+ *       [] -> []
+ *     | (i,n) :: tl ->
+ *        if i = prev
+ *        then n :: aux i tl
+ *        else aux i tl
+ *   in
+ *   aux 0 l;;
+ * 
+ * 
+ * let lol = List.map ~f:permute (List.map ~f:digits primes);;
+ * 
+ * let rofl = List.map ~f:(fun x -> List.map ~f:combine x) lol;;
+ * 
+ * let foo = List.map ~f:(fun x -> List.filter ~f:(fun y -> y > 1000) x) rofl;;
+ * 
+ * let bar = List.map ~f:(fun x -> List.filter ~f:is_prime x) foo;;
+ * 
+ * let quux = List.filter ~f:(fun x -> List.length x >= 3) bar;;
+ *)
